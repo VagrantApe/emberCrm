bbdd11bf48181cf17d784ddd2895dfcb0c09af58
John Johnson <luckstedcraig@gmail.com>
2014-08-14 16:42:05 -0500

    showing leads

--- app/serializers/lead_serializer.rb
+++ app/serializers/lead_serializer.rb
@@ -1,3 +1,3 @@
 class LeadSerializer < ActiveModel::Serializer
-  attribites :id, :first_name, :last_name, :email, :phone, :status, :notes
+  attributes :id, :first_name, :last_name, :email, :phone, :status, :notes
 end
